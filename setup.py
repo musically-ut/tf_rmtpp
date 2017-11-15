c015b6cca12278cc91a644fe72be0438daa2bd37
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-11-16 00:45:24 +0100

    Cleanup files and versions supported.

--- setup.py
+++ setup.py
@@ -18,10 +18,7 @@ setuptools.setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
