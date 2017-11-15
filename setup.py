565432ebca1f8dcc895468c7d4f781bf4a033d52
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-11-15 21:27:26 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="tf_rmtpp",
+    version="0.1.0",
+    url="https://github.com/musically-ut/tf_rmtpp",
+
+    author="Utkarsh Upadhyay",
+    author_email="musically.ut@gmail.com",
+
+    description="Recurrent Marked Temporal Point Processes in Tensorflow",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
---
+++ tf_rmtpp/__init__.py
@@ -0,0 +1,5 @@
+"""tf_rmtpp - Recurrent Marked Temporal Point Processes in Tensorflow"""
+
+__version__ = '0.1.0'
+__author__ = 'Utkarsh Upadhyay <musically.ut@gmail.com>'
+__all__ = []
