92a2e9069095b7ce9e55ef1dd71006b69721aee2
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-11-24 08:15:23 +0100

    Fix imports.

--- src/tf_rmtpp/__init__.py
+++ src/tf_rmtpp/__init__.py
@@ -1,5 +1,8 @@
 """tf_rmtpp - Recurrent Marked Temporal Point Processes in TensorFlow"""
 
+from .rmtpp import RMTPP
+from .utils import read_data
+
 __version__ = '0.1.0'
 __author__ = 'Utkarsh Upadhyay <musically.ut@gmail.com>'
 __all__ = []
